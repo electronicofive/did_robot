d76cd1ca1bf50e0bebea0915c92fc4d39b5c550f
Juan D. Vergara <electronico5@octopusforce.com>
2025-03-01 09:44:23 -0500

    Update robot description parser

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -47,13 +47,15 @@ def generate_launch_description():
 
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    print(robot_description)
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[controller_params_file]
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
@@ -83,24 +85,6 @@ def generate_launch_description():
             on_start=[joint_broad_spawner],
         )
     )
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
 
 
     # Launch them all!
